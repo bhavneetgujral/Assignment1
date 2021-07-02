d769d73fe0835d0d2c14dd344307e5b299a679be
bhavneetgujral <bhaveegujral08@gmail.com>
2021-07-02 16:14:38 +1200

    commiting files to local

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            SecurityService securityService = new SecurityService();
+            return securityService.IsValid(userModel) ? View("LoginSuccess", userModel) : View("LoginFailure", userModel);
+        }
+    }
+}
---
+++ WebApplication1/Services/SecurityService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class SecurityService
+    {
+       
+        UsersDAO usersDAO = new UsersDAO();
+
+        public SecurityService()
+        {
+           
+        }
+        public bool IsValid(UserModel user)
+        {
+            return usersDAO.FindUserByNameAndPassword(user);
+            //return positive if found in the list
+            
+        }
+    }
+}
